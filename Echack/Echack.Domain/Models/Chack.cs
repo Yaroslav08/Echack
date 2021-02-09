523376e4fe1ea1836ef19a98b4d5b07e3584e74c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-09 17:51:58 +0200

    Added Chack model

---
+++ Echack/Echack.Domain/Models/Chack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace Echack.Domain.Models
+{
+    public class Chack : BaseModelWithIdentityGen<Guid>
+    {
+        [Required, MinLength(1), MaxLength(100)]
+        public string ShopName { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        public string Products { get; set; }
+        [Required]
+        public bool IsImportant { get; set; } = false;
+    }
+}
