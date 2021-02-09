2f9ed10fd5b3ec7771c9877acebc05e69486cf9a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-09 19:47:49 +0200

    Added DbContext

---
+++ Echack/Echack.Infrastructure.Data/Context/EchackContext.cs
@@ -0,0 +1,21 @@
+﻿using Echack.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Infrastructure.Data.Context
+{
+    public class EchackContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chack> Chacks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EchackDb;Trusted_Connection=True;");
+        }
+    }
+}
