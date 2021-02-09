b300ece29726a83c66c20c415115acb536c4c205
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-09 17:39:15 +0200

    Added User model

---
+++ Echack/Echack.Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace Echack.Domain.Models
+{
+    public class User : BaseModelWithIdentityGen<int>
+    {
+        [Required]
+        public string Name { get; set; }
+        [MinLength(20), MaxLength(150)]
+        public string Avatar { get; set; }
+        [MinLength(8), MaxLength(100)]
+        public string Login { get; set; }
+        [MinLength(8), MaxLength(2500)]
+        public string PasswordHash { get; set; }
+        [Required, MaxLength(50)]
+        public string Role { get; set; }
+    }
+}
