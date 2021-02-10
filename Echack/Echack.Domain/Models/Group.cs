c6ad1e31631c49487ccfb8413cde05edd4a469c4
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-10 22:31:12 +0200

    Added Group model

---
+++ Echack/Echack.Domain/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Domain.Models
+{
+    public class Group : BaseModelWithIdentityGen<Guid>
+    {
+        [Required, MinLength(1), MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string Color { get; set; } = "#6495ED";
+        [MinLength(10), MaxLength(250)]
+        public string Desc { get; set; }
+    }
+}
