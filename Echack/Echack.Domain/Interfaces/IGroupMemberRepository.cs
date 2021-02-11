929e182605dca9589138f4fcbdbcfb2f2123bb30
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-11 20:34:31 +0200

    Added repositories

---
+++ Echack/Echack.Domain/Interfaces/IGroupMemberRepository.cs
@@ -0,0 +1,14 @@
+﻿using Echack.Domain.Models;
+using EFRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Domain.Interfaces
+{
+    public interface IGroupMemberRepository : ICRUDRepository<GroupMember, Guid>
+    {
+
+    }
+}
