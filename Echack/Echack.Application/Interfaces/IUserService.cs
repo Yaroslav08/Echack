ee0223f636b52beb25bea6716a6fedc64cdd1fce
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-12 21:12:00 +0200

    Added IUserService

---
+++ Echack/Echack.Application/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Echack.Application.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserViewModel> GetUserById(int id);
+        Task<List<List<UserViewModel>>> SearchUsers(string user, int afterId);
+    }
+}
