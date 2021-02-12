4259799f4d12e629bcf83d09bea7e794973f03ed
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-12 21:10:07 +0200

    Added methods for getting chacks by params

--- Echack/Echack.Domain/Interfaces/IChackRepository.cs
+++ Echack/Echack.Domain/Interfaces/IChackRepository.cs
@@ -9,6 +9,8 @@ namespace Echack.Domain.Interfaces
 {
     public interface IChackRepository : ICRUDRepository<Chack, Guid>
     {
-
+        Task<List<Chack>> GetChacksByMounthAsync(int ownerId, int mounth);
+        Task<List<Chack>> GetChacksByTimeAsync(int ownerId, DateTime from, DateTime to);
+        Task<List<Chack>> GetChacksByShopNameAsync(int ownerId, string shopName, int afterId);
     }
 }
--- Echack/Echack.Infrastructure.Data/Repositories/ChackRepository.cs
+++ Echack/Echack.Infrastructure.Data/Repositories/ChackRepository.cs
@@ -2,6 +2,7 @@
 using Echack.Domain.Models;
 using Echack.Infrastructure.Data.Context;
 using EFRepository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,30 @@ namespace Echack.Infrastructure.Data.Repositories
             db = _db;
         }
 
+        public async Task<List<Chack>> GetChacksByMounthAsync(int ownerId, int mounth)
+        {
+            return await db.Chacks.AsNoTracking()
+                .Where(d => d.UserId == ownerId && d.CreatedAt.Month == mounth && d.CreatedAt.Year == DateTime.Now.Year)
+                .ToListAsync();
+        }
+
+        public async Task<List<Chack>> GetChacksByShopNameAsync(int ownerId, string shopName, int skip = 0)
+        {
+            if (skip == 0)
+                return await db.Chacks.AsNoTracking().Where(d => d.UserId == ownerId && d.ShopName.Contains(shopName))
+                    .Take(20)
+                    .ToListAsync();
+            return await db.Chacks.AsNoTracking().Where(d => d.UserId == ownerId && d.ShopName.Contains(shopName))
+                    .Take(20)
+                    .Skip(skip)
+                    .ToListAsync();
+        }
+
+        public async Task<List<Chack>> GetChacksByTimeAsync(int ownerId, DateTime from, DateTime to)
+        {
+            return await db.Chacks.AsNoTracking()
+                .Where(d => d.UserId == ownerId && d.CreatedAt >= from && d.CreatedAt <= to)
+                .ToListAsync();
+        }
     }
 }
