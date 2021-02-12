642166f8b2bb136124ff5579599113ad0591422d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-12 21:14:58 +0200

    Added IUnitOfWork

---
+++ Echack/Echack.Infrastructure.Data/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Echack.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IUserRepository userRepository;
+
+
+        public UnitOfWork(IUserRepository _userRepository)
+        {
+            userRepository = _userRepository;
+        }
+
+
+        public IUserRepository UserRepository => userRepository;
+    }
+}
