a5037a2f42bf4a629e0ef007721d98107276f3a1
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-12 21:38:53 +0200

    Some changed

---
+++ Echack/Echack.Web/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Echack.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Echack.Web.Controllers
+{
+    public class UsersController : BaseController
+    {
+        IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            return Ok(await _userService.GetUserById(id));
+        }
+
+    }
+}
