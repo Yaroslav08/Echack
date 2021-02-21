bb99517f927495d71452bb95a8bb0dbdd9bfe043
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-21 17:02:05 +0200

    Added ChackEditViewModel

---
+++ Echack/Echack.Application/ViewModels/Chack/ChackEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Echack.Application.ViewModels.Chack
+{
+    public class ChackEditViewModel : RequestModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required, MinLength(1), MaxLength(100)]
+        public string ShopName { get; set; }
+        public bool IsImportant { get; set; }
+        public double TotalPrice { get; set; }
+        public List<ProductCreateViewModel> Products { get; set; }
+    }
+}
--- Echack/Echack.Application/ViewModels/RequestModel.cs
+++ Echack/Echack.Application/ViewModels/RequestModel.cs
@@ -8,6 +8,6 @@ namespace Echack.Application.ViewModels
     public class RequestModel
     {
         public string IP { get; set; }
-        public int Id { get; set; }
+        public int UserId { get; set; }
     }
 }
