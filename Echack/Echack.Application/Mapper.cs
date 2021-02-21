080c4393658f5eeb378af99c50a600acd49a5cd6
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-21 17:41:25 +0200

    Some fixed in mapper

--- Echack/Echack.Application/Mapper.cs
+++ Echack/Echack.Application/Mapper.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Echack.Application.ViewModels.Chack;
+using Echack.Application.ViewModels.Group;
 using Echack.Application.ViewModels.User;
 using Echack.Domain.Models;
 using System;
@@ -18,9 +19,13 @@ namespace Echack.Application
                 .ForMember(d => d.Products, s => s.MapFrom(d => GetProducts(d.Products)))
                 .ForMember(d => d.Group, s => s.MapFrom(d => d.Group))
                 .ForMember(d => d.User, s => s.MapFrom(d => d.User));
+            CreateMap<Chack, ChackGroupViewModel>();
 
             CreateMap<User, UserViewModel>();
             CreateMap<User, UserChackViewModel>();
+
+
+            CreateMap<Group, GroupViewModel>();
         }
 
 
