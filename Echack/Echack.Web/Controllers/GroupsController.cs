fbb3cb674bc4b6543cb4136d1c8459d3c9399a74
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-21 17:39:13 +0200

    Released IGroupService in controller

---
+++ Echack/Echack.Web/Controllers/GroupsController.cs
@@ -0,0 +1,33 @@
+﻿using Echack.Application.Interfaces;
+using Echack.Application.ViewModels.Group;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Echack.Web.Controllers
+{
+    public class GroupsController : BaseController
+    {
+        private readonly IGroupService _groupService;
+        public GroupsController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGroupById(Guid id)
+        {
+            var group = await _groupService.GetGroupById(id);
+            return Ok(group);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup([FromBody] GroupCreateViewModel model)
+        {
+            var result = await _groupService.CreateGroup(model);
+            return Ok(result);
+        }
+    }
+}
