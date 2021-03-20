382cdf8b75086e9ad9f15ba3d270ed8e170ddb6b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-20 22:18:11 +0200

    Added common Result in service

---
+++ Ereceipt/Ereceipt.Application/Results/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.Results
+{
+    public class Result<T> where T : class
+    {
+        public bool OK { get; } = true;
+
+        public string Error { get; }
+
+        public Exception Exception { get; }
+
+        public T Data { get; }
+
+        public Result(T data) : this(data,null,null)
+        {
+
+        }
+
+        public Result(string error, Exception ex = null) : this(null,error, ex)
+        {
+            OK = false;
+            Error = error;
+        }
+
+        public Result(Exception ex, string error = null) : this(null, error, ex)
+        {
+            OK = false;
+            Exception = ex;
+        }
+
+        public Result(T data, string error, Exception ex)
+        {
+            if (data == null || !string.IsNullOrEmpty(error) || ex != null)
+            {
+                OK = false;
+                Error = error;
+                Exception = ex;
+                return;
+            }
+            OK = true;
+            Data = data;
+        }
+    }
+}
