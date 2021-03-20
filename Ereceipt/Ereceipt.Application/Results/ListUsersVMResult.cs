f5a6b99d6ff4a98396626c4a3453dc5ef25a2504
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-20 22:32:26 +0200

    Added results for list of users and user view models

---
+++ Ereceipt/Ereceipt.Application/Results/ListUsersVMResult.cs
@@ -0,0 +1,14 @@
+﻿using Ereceipt.Application.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ereceipt.Application.Results
+{
+    public class ListUsersVMResult : Result<List<UserViewModel>>
+    {
+        public ListUsersVMResult(List<UserViewModel> users) : base(users) { }
+    }
+}
