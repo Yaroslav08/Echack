3cd92a5f5bd694c8c1f539a8beeb456f177a09be
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-20 23:21:09 +0200

    Added GroupResult and list

---
+++ Ereceipt/Ereceipt.Application/Results/Groups/GroupResult.cs
@@ -0,0 +1,13 @@
+﻿using Ereceipt.Application.ViewModels.Group;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.Results.Groups
+{
+    public class GroupResult : Result<GroupViewModel>
+    {
+        public GroupResult(GroupViewModel group) : base(group) { }
+    }
+}
---
+++ Ereceipt/Ereceipt.Application/Results/Groups/ListGroupResult.cs
@@ -0,0 +1,13 @@
+﻿using Ereceipt.Application.ViewModels.Group;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.Results.Groups
+{
+    public class ListGroupResult : Result<List<GroupViewModel>>
+    {
+        public ListGroupResult(List<GroupViewModel> groups) : base(groups) { }
+    }
+}
