4d02f08620594d47bdc8bae0d6bf5728cfe406de
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-04-02 23:53:26 +0300

    Added UpdateAt fields to some models

--- Ereceipt/Ereceipt.Application/ViewModels/GroupMember/GroupMemberViewModel.cs
+++ Ereceipt/Ereceipt.Application/ViewModels/GroupMember/GroupMemberViewModel.cs
@@ -6,8 +6,10 @@ namespace Ereceipt.Application.ViewModels.GroupMember
     public class GroupMemberViewModel
     {
         public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string Title { get; set; }
         public GroupViewModel Group { get; set; }
         public UserReceiptViewModel User { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
