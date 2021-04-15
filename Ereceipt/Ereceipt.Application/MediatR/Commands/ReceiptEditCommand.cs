e34a70558c6bab6d48b84b351d70d179a40bb45f
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-04-15 11:59:22 +0300

    Added Mediatr handler for edit receipt

---
+++ Ereceipt/Ereceipt.Application/MediatR/Commands/ReceiptEditCommand.cs
@@ -0,0 +1,33 @@
+﻿using Ereceipt.Application.Interfaces;
+using Ereceipt.Application.Results.Receipts;
+using Ereceipt.Application.ViewModels.Receipt;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.MediatR.Commands
+{
+    public class ReceiptEditCommand : IRequest<ReceiptResult>
+    {
+        public ReceiptEditViewModel Receipt { get; set; }
+
+        public ReceiptEditCommand(ReceiptEditViewModel receipt)
+        {
+            Receipt = receipt;
+        }
+    }
+
+    public class ReceiptEditCommandHandler : IRequestHandler<ReceiptEditCommand, ReceiptResult>
+    {
+        IReceiptService _ReceiptService;
+        public ReceiptEditCommandHandler(IReceiptService receiptService)
+        {
+            _ReceiptService = receiptService;
+        }
+
+
+        public async Task<ReceiptResult> Handle(ReceiptEditCommand request, CancellationToken cancellationToken)
+        {
+            return await _ReceiptService.EditReceipt(request.Receipt);
+        }
+    }
+}
