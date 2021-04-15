3b19746d48553f17089741bae77f2af6036afca4
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-04-15 19:06:47 +0300

    Куьщмув дштуі шт ИфіуЬщвуд сдфіі

--- Ereceipt/Ereceipt.Domain/Models/BaseModel.cs
+++ Ereceipt/Ereceipt.Domain/Models/BaseModel.cs
@@ -3,18 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Ereceipt.Domain.Models
 {
-
     public class BaseModel
     {
         [Required]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string CreatedBy { get; set; }
-
-
         public DateTime? UpdatedAt { get; set; }
         public string UpdatedBy { get; set; }
     }
-
     public class BaseModel<TypeId> : BaseModel
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
