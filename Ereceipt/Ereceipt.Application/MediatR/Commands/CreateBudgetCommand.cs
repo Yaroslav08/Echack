491738895a36f65bce56317386e7b71a6e3b496b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-10 17:45:23 +0300

    Added CreateBudgetCommand

---
+++ Ereceipt/Ereceipt.Application/MediatR/Commands/CreateBudgetCommand.cs
@@ -0,0 +1,30 @@
+﻿using Ereceipt.Application.Results.Budgets;
+using Ereceipt.Application.Services.Interfaces;
+using Ereceipt.Application.ViewModels.Budget;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.MediatR.Commands
+{
+    public class CreateBudgetCommand : IRequest<BudgetResult>
+    {
+        public CreateBudgetCommand(BudgetCreateViewModel model)
+        {
+            Budget = model;
+        }
+        public BudgetCreateViewModel Budget { get; set; }
+    }
+    public class CreateBudgetCommandHandler : IRequestHandler<CreateBudgetCommand, BudgetResult>
+    {
+        private readonly IBudgetService _budgetService;
+        public CreateBudgetCommandHandler(IBudgetService budgetService)
+        {
+            _budgetService = budgetService;
+        }
+
+        public async Task<BudgetResult> Handle(CreateBudgetCommand request, CancellationToken cancellationToken)
+        {
+            return await _budgetService.CreateBudgetAsync(request.Budget);
+        }
+    }
+}
--- Ereceipt/Ereceipt.Application/MediatR/Queries/GetBudgetByIdQuery.cs
+++ Ereceipt/Ereceipt.Application/MediatR/Queries/GetBudgetByIdQuery.cs
@@ -2,9 +2,6 @@
 using Ereceipt.Application.Services.Interfaces;
 using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 namespace Ereceipt.Application.MediatR.Queries
