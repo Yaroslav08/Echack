5086d391793231b692e2fae9c9742c3790133712
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-10 17:59:33 +0300

    Added BudgetController

---
+++ Ereceipt/Ereceipt.Web/Controllers/BudgetsController.cs
@@ -0,0 +1,24 @@
+﻿using Ereceipt.Application.Extensions;
+using Ereceipt.Application.MediatR.Commands;
+using Ereceipt.Application.ViewModels.Budget;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+namespace Ereceipt.Web.Controllers
+{
+    public class BudgetsController : ApiController
+    {
+        private readonly IMediator _mediator;
+        public BudgetsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBudget([FromBody] BudgetCreateViewModel model)
+        {
+            model.IncomeRequestInit(GetId(), GetIpAddress());
+            var result = await _mediator.Send(new CreateBudgetCommand(model));
+            return Result(result);
+        }
+    }
+}
