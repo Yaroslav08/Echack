e1d2421d097709184bad805b58c95678c8068592
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-27 00:15:34 +0300

    Added new controller for work with BudgetCategories

---
+++ Ereceipt/Ereceipt.Web/Controllers/BudgetCategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using Ereceipt.Application.ViewModels.BudgetCategory;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ereceipt.Application.Extensions;
+using Ereceipt.Application.MediatR.Commands;
+
+namespace Ereceipt.Web.Controllers
+{
+
+    public class BudgetCategoriesController : ApiController
+    {
+        private readonly IMediator _mediator;
+        public BudgetCategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("new-receipt")]
+        public async Task<IActionResult> AddReceiptToBudgetCategory([FromBody] BudgetReceiptCreateModel model)
+        {
+            model.IncomeRequestInit(GetId(), GetIpAddress());
+            var result = await _mediator.Send(new AddReceiptToBudgetCategoryCommand(model));
+            return Result(result);
+        }
+
+        [HttpDelete("remove-receipt")]
+        public async Task<IActionResult> RemoveReceiptFromBudgetCategory([FromBody] BudgetReceiptRemoveModel model)
+        {
+            model.IncomeRequestInit(GetId(), GetIpAddress());
+            var result = await _mediator.Send(new RemoveReceiptFromBudgetCategoryCommand(model));
+            return Result(result);
+        }
+    }
+}
