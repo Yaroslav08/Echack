c6b993d6dc48674aab8618b602e74bbb9d77ad5e
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-27 00:31:03 +0300

    Added some commands and queries for BudgetCategory

---
+++ Ereceipt/Ereceipt.Application/MediatR/Commands/CreateBudgetCategoryCommand.cs
@@ -0,0 +1,32 @@
+﻿using Ereceipt.Application.Results.BudgetCategories;
+using Ereceipt.Application.Services.Interfaces;
+using Ereceipt.Application.ViewModels.BudgetCategory;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Ereceipt.Application.MediatR.Commands
+{
+    public class CreateBudgetCategoryCommand : IRequest<BudgetCategoryResult>
+    {
+        public BudgetCategoryCreateModel BudgetCategory { get; set; }
+
+        public CreateBudgetCategoryCommand(BudgetCategoryCreateModel budgetCategory)
+        {
+            BudgetCategory = budgetCategory;
+        }
+    }
+
+    public class CreateBudgetCategoryCommandHandler : IRequestHandler<CreateBudgetCategoryCommand, BudgetCategoryResult>
+    {
+        private readonly IBudgetCategoryService _budgetCategoryService;
+        public CreateBudgetCategoryCommandHandler(IBudgetCategoryService budgetCategoryService)
+        {
+            _budgetCategoryService = budgetCategoryService;
+        }
+
+        public async Task<BudgetCategoryResult> Handle(CreateBudgetCategoryCommand request, CancellationToken cancellationToken)
+        {
+            return await _budgetCategoryService.CreateBudgetCategoryAsync(request.BudgetCategory);
+        }
+    }
+}
